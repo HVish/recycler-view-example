0076ba04253c83e1dcdc3ec0f5ac668ea5fac8fa
Vishnu Singh <vishnusingh1729@gmail.com>
2017-04-18 16:38:17 +0530

    data fetch from server complete

--- app/src/main/java/dashboard/vishnu/com/dashboard/Activities/MainActivity.java
+++ app/src/main/java/dashboard/vishnu/com/dashboard/Activities/MainActivity.java
@@ -1,21 +1,39 @@
 package dashboard.vishnu.com.dashboard.Activities;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import dashboard.vishnu.com.dashboard.Adapters.ShipmentAdapter;
+import dashboard.vishnu.com.dashboard.Config;
 import dashboard.vishnu.com.dashboard.Models.Shipment;
 import dashboard.vishnu.com.dashboard.R;
+import dashboard.vishnu.com.dashboard.Utils.AppController;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
 
     //recyclerview objects
     private RecyclerView recyclerView;
@@ -24,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
 
     //model object for our list data
     private List<Shipment> shipments;
+
+    // progress dialog
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        progress = new ProgressDialog(this);
+        progress.setMessage("Please wait...");
+        progress.setCancelable(false);
+
         shipments = new ArrayList<>();
 
         //loading list view item with this function
@@ -42,71 +67,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadShipments() {
-        //you can fetch the data from server or some apis
-        try {
-            JSONObject shipment = new JSONObject("{\n" +
-                    "        \"shipmentId\": 7191,\n" +
-                    "        \"generatedId\": \"SP17041271914\",\n" +
-                    "        \"shipperId\": \"SR1704112913\",\n" +
-                    "        \"userId\": 732,\n" +
-                    "        \"transporterId\": \"TR1704114913\",\n" +
-                    "        \"pickupDate\": \"2017-04-11T18:30:00.000Z\",\n" +
-                    "        \"bidClosingDate\": \"2017-04-12T16:51:00.000Z\",\n" +
-                    "        \"advance\": 60,\n" +
-                    "        \"balance\": \"pahuch\",\n" +
-                    "        \"truckType\": 17,\n" +
-                    "        \"truckCount\": 1,\n" +
-                    "        \"dala\": \"open\",\n" +
-                    "        \"extraHeight\": 10,\n" +
-                    "        \"expectedAmount\": 7800,\n" +
-                    "        \"reverseBid\": null,\n" +
-                    "        \"remarks\": \"Details on call\",\n" +
-                    "        \"insurance\": \"yes\",\n" +
-                    "        \"assistance\": \"yes\",\n" +
-                    "        \"paymentProcessId\": 1,\n" +
-                    "        \"createdAt\": \"2017-04-12T08:51:36.000Z\",\n" +
-                    "        \"status\": \"accepted\",\n" +
-                    "        \"truck\": \"11 Ton (2016 etc.)\",\n" +
-                    "        \"shipper\": \"Bharat Sarkaar\",\n" +
-                    "        \"transporter\": \"Vaahika\",\n" +
-                    "        \"commission\": 0,\n" +
-                    "        \"pickup\": \"VBN#Hingoli\",\n" +
-                    "        \"delivery\": \"VKI#Jaipur\",\n" +
-                    "        \"items\": [\n" +
-                    "          {\n" +
-                    "            \"shipmentId\": 7191,\n" +
-                    "            \"capsuleId\": null,\n" +
-                    "            \"itemId\": 7517,\n" +
-                    "            \"name\": \"Cotton\",\n" +
-                    "            \"weight\": 20,\n" +
-                    "            \"quantity\": 50\n" +
-                    "          }\n" +
-                    "        ],\n" +
-                    "        \"bids\": [\n" +
-                    "          {\n" +
-                    "            \"transporterId\": 491,\n" +
-                    "            \"shipmentId\": 7191,\n" +
-                    "            \"bidId\": 8366,\n" +
-                    "            \"quote\": 23423,\n" +
-                    "            \"oldQuote\": 23423,\n" +
-                    "            \"remarks\": \"good\",\n" +
-                    "            \"offerAction\": null,\n" +
-                    "            \"createdAt\": \"2017-04-12T12:50:14.000Z\",\n" +
-                    "            \"status\": \"accepted\",\n" +
-                    "            \"transporter\": \"TR1704114913\",\n" +
-                    "            \"rating\": 0\n" +
-                    "          }\n" +
-                    "        ]}");
 
-            for (int i = 1; i <= 5; i++) {
-                Shipment shipmentData = new Shipment(shipment);
-                shipments.add(shipmentData);
+        showProgressDialog();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                Config.DASHBOARD_URL + "?tab=older", null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+
+                try {
+                    JSONObject result = response.getJSONObject("result");
+                    JSONArray shipmentsJSON = result.getJSONArray("tab");
+                    for (int i = 0; i < shipmentsJSON.length(); i++) {
+                         shipments.add(new Shipment(shipmentsJSON.getJSONObject(i)));
+                    }
+                    adapter = new ShipmentAdapter(shipments, MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+                hideProgressDialog();
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        }, new Response.ErrorListener() {
 
-        adapter = new ShipmentAdapter(shipments, this);
-        recyclerView.setAdapter(adapter);
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(MainActivity.this.getApplicationContext(),
+                        networkResponse.statusCode, Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+                hideProgressDialog();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "JWT " + "");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
+    }
+
+    private void showProgressDialog() {
+        if (!progress.isShowing())
+            progress.show();
+    }
+
+    private void hideProgressDialog() {
+        if (progress.isShowing())
+            progress.dismiss();
     }
 }
---
+++ app/src/main/java/dashboard/vishnu/com/dashboard/Config.java
@@ -0,0 +1,10 @@
+package dashboard.vishnu.com.dashboard;
+
+/**
+ * Created by vishnu on 18/4/17.
+ */
+
+public class Config {
+    public static final String BASE_URL = "https://client.vaahika.com/";
+    public static final String DASHBOARD_URL = BASE_URL + "api/dashboard";
+}
